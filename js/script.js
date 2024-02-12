71098f160a84c0f88b0a6808f0fecf361e568e87
choinseong <choinseong2237@gmail.com>
2024-02-12 14:36:25 +0900

    날짜 어쩌구저쩌구

--- js/script.js
+++ js/script.js
@@ -19,3 +19,29 @@ function playCarousel() {
     const myCarouselElement = document.querySelector('#carouselExampleIndicators')
     $(myCarouselElement).carousel('cycle');
 }
+
+// Reservation
+function reservation() {
+    // JSON 파일에서 데이터 불러오기
+    async function fetchReservation() {
+        const getReservationJSON = await fetch("../api/reservation.json");
+        const reservationJSON = await getReservationJSON.json();
+        return reservationJSON["reservition"];
+    }
+    // const reservationData =  fetchReservation();
+    // console.log(reservationData);
+
+    for(let i = 0 ; i < 14; i++) {
+        const date = new Date();
+        date.setDate(date.getDate() + i);
+        const year = date.getFullYear();
+        const month = ("0" + (date.getMonth() + 1)).slice(-2);
+        const day = ("0" + (date.getDate())).slice(-2);
+        let week = date.getDay();
+    
+        // 조건식 ? 참일 떄의 값 : 거짓일 떄의 값;
+        week = ((week == 0) || (week == 6)) ? "주말" : "주중";
+    
+        console.log(`${year}.${month}.${day} (${week})`);
+    }
+}
