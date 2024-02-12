46265ab84afd2c62711c6622eb4daf0683575381
choinseong <choinseong2237@gmail.com>
2024-02-12 17:47:48 +0900

    주석

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function reservation() {
     // console.log(reservationData);
     const resDateTabelElem = document.querySelector("#resDateTable");
     
-
+    // 테이블에 맨 윗줄 가로 부분이고 날짜를 차례대로 출력
     for(let i = 0 ; i < 14; i++) {
         const date = new Date();
         date.setDate(date.getDate() + i);
@@ -48,6 +48,8 @@ function reservation() {
         // 토요일 - 글자 색을 파란색으로 변경하는 class가 추가되야 함
         // 일요일 - 글자 색을 빨간색으로 변경하는 class가 추가되야 함
 
+        // innerHTML로 자바스크립트 언어를 HTML로 만들어 추가
+        // 6은 토요일 0은 일요일 그외는 평일로 요일마다 색을 바꾸는 코드
         if(week == 6) {
             resDateTabelElem.innerHTML += `<td class='b'>${year}.${month}.${day}</td>`;
         }else if (week == 0) {
@@ -57,29 +59,32 @@ function reservation() {
         }
         
     }
-    
+    // 이건 뭐지
     for (let i = 0; i < 17; i++){
         for (let k = 0; k < 14; k++){
             document.querySelector(`#row${i}`).innerHTML += `<td></td>`;
         }
     }
-
+    // 테이블 예약 기능 일부 
     async function updateReservation() {
-        const reservation = await fetchReservation();        
+        const reservation = await fetchReservation();
         for (let i = 0; i < 17; i++){
             for (let k = 0; k < 14; k++){
+                // data변수는 json파일의 데이터의 값들을 얻어 도형으로 바꾸기 위해서
                 const data = reservation[k][`D+${k}`][i]["status"];
-                const tdElen = document.querySelector(`#row${i} > td:nth-of-type(${k + 2})`);
+                // row${i}는 tr이므로 tr안 td를 가상선택자로 시작지점을 찾는코드
+                const tdElem = document.querySelector(`#row${i} > td:nth-of-type(${k + 2})`);
                 
+                // 데이터가 W면 ●로 HTML을 변경 그리고 className으로 class를 지정해 css를 먹이는 코드
                 if (data == "W") {
-                    tdElen.innerText = "●";
-                    tdElen.className = "w";
+                    tdElem.innerText = "●";
+                    tdElem.className = `w D+${k} ${i}`; // 번지수를 알기 위해서 클래스 이름으로 알기쉽게 
                 }else if (data == "R") {
-                    tdElen.innerText = "▲";
-                    tdElen.className = "r";
+                    tdElem.innerText = "▲";
+                    tdElem.className = `r D+${k} ${i}`;
                 }else {
-                    tdElen.innerText = "■";
-                    tdElen.className = "c";
+                    tdElem.innerText = "■";
+                    tdElem.className = `c D+${k} ${i}`;
                 }
                 
                 
